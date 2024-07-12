62f0450b3ddd7144ab3751d5ea48fce84a3d6974
Peter Seid <pwseid@gmail.com>
2024-07-12 14:32:15 -0700

    Adds support for setting ColorSlider options in SwiftUI

--- Sources/ColorSliderView.swift
+++ Sources/ColorSliderView.swift
@@ -2,13 +2,23 @@ import SwiftUI
 
 public struct ColorSliderView: UIViewRepresentable {
     @Binding var selectedColor: UIColor
+    var orientation: Orientation
+    var previewSide: DefaultPreviewView.Side
+    var borderWidth: CGFloat
+    var borderColor: UIColor
 
-    public init(selectedColor: Binding<UIColor>) {
+    public init(selectedColor: Binding<UIColor>, orientation: Orientation = .horizontal, previewSide: DefaultPreviewView.Side = .top, borderWidth: CGFloat = 0, borderColor: UIColor = .clear) {
         self._selectedColor = selectedColor
+        self.orientation = orientation
+        self.previewSide = previewSide
+        self.borderWidth = borderWidth
+        self.borderColor = borderColor
     }
 
     public func makeUIView(context: Context) -> ColorSlider {
-        let colorSlider = ColorSlider(orientation: .vertical, previewSide: .left)
+        let colorSlider = ColorSlider(orientation: orientation, previewSide: previewSide)
+        colorSlider.gradientView.layer.borderWidth = borderWidth
+        colorSlider.gradientView.layer.borderColor = borderColor.cgColor
         colorSlider.addTarget(context.coordinator, action: #selector(Coordinator.colorChanged(_:)), for: .valueChanged)
         return colorSlider
     }
