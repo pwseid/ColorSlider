7333fd01a6a916641a6f88611fc9a03fb076f675
Peter Seid <pwseid@gmail.com>
2024-07-12 14:00:38 -0700

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ColorSlider",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(
+            name: "ColorSlider",
+            targets: ["ColorSlider"]),
+    ],
+    targets: [
+        .target(
+            name: "ColorSlider",
+            path: "Sources"),
+        .testTarget(
+            name: "ColorSliderTests",
+            dependencies: ["ColorSlider"],
+            path: "Tests"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
